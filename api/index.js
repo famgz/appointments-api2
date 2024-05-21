6dc230a5c2aecd08d467156accc518aa4f68c724
famgz <famgz@proton.me>
2024-05-21 20:46:56 -0300

    chore: established connection with Amazon RDS databse. it's running locally on port 3000 with no problems

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const app = require('../app');
+const app = require('../app').default;
 module.exports = (req, res) => {
   app(req, res);
 };
--- infra/db.js
+++ infra/db.js
@@ -1,16 +1,33 @@
-const Sequelize = require("sequelize");
-const sqlite3 = require('sqlite3').verbose(); // Explicitly require sqlite3
-const path = require('path');
-const fs = require('fs');
+const Sequelize = require('sequelize');
 
-const storagePath = path.resolve(__dirname, 'database.sqlite');
-
-fs.chmodSync(storagePath, 0o666);
-
+// Create Sequelize instance
 const sequelize = new Sequelize({
-  dialect: "sqlite",
-  dialectModule: sqlite3, // Pass sqlite3 module to Sequelize
-  storage: storagePath,
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: console.log,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  dialectOptions: {
+    connectTimeout: 60000, // 60 seconds
+  },
 });
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
