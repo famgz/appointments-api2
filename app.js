609df63de17fb1b100e93a241934ff8f77485799
famgz <famgz@proton.me>
2024-05-22 12:44:39 -0300

    fix: add swagger custom css cdn url

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const express = require('express');
 const cors = require('cors');
 
 const SWAGGER_CSS_URL =
-  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.12/swagger-ui.min.css';
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
-swaggerUi.setup({ customCssUrl: SWAGGER_CSS_URL });
 
 const router = require('./router');
 
@@ -20,7 +19,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(
+  '/doc',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile, { customCssUrl: SWAGGER_CSS_URL })
+);
 
 if (process.env.NODE_ENV === 'development') {
   app.listen(3000, () => console.log('Server running on port 3000'));
